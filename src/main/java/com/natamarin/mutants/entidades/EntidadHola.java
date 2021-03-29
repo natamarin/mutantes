98275414e90a8672399a107cdfb0c4c1913ef639
nataMarin <natamarin6@gmail.com>
2021-03-28 21:23:59 -0500

    Se modifica entidad de prueba

--- src/main/java/com/natamarin/mutants/entidades/EntidadHola.java
+++ src/main/java/com/natamarin/mutants/entidades/EntidadHola.java
@@ -1,10 +1,15 @@
-package com.natamarin.mutants.Entidades;
+package com.natamarin.mutants.entidades;
 
 import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 public class EntidadHola {
 
@@ -14,6 +19,9 @@ public class EntidadHola {
 		
 		private static final long serialVersionUID = 1L;
 
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+		@GenericGenerator(name = "native", strategy = "native")
 		@Column(name = "IDPER")
 		private Long idPersona;
 
